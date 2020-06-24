19ad5b3143369c6f8d92f52860734c7061abb41f
Simon <trilogicworlds@gmail.com>
2020-06-24 18:29:01 +0200

    add for loop demos

---
+++ ForLoop.java
@@ -0,0 +1,48 @@
+import java.io.IOException;
+
+public class ForLoop {
+    public static void main(String[] args) throws IOException {
+        int i, j;
+
+        for (i = 0, j = 10; i < j; i++, j--) { // support for multiple values
+            System.out.println("i and j: " + i + " " + j);
+        }
+
+        // loop until S is typed
+        int k;
+
+        System.out.println("Print S to stop.");
+
+        for (k = 0; (char) System.in.read() != 'S'; k++) {
+            System.out.println("Pass #: " + k);
+        }
+
+        // support for missing pieces
+        int l;
+
+        for (l = 0; l < 10;) {
+            System.out.println("Pass #: " + l);
+            l++;
+        }
+
+        // you can even do this as a while loop
+        for (; i < 10;) { // must be declared somewhere else
+            System.out.println("i is less than ten");
+            i++;
+        }
+
+        /* e.g. infinite loop
+        // for (;;) {
+            // do something
+        } */
+
+        // empty for loop
+        int m;
+        int sum = 0;
+
+        for (m = 1; m <= 5; sum += m++) {
+
+        }
+        System.out.println("Sum is: " + sum); // note you can't print m, it is outside of scope
+    }
+}
