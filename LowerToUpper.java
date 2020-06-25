ab5f6460c688ac5f451f37856ba36917066f7f06
Simon <trilogicworlds@gmail.com>
2020-06-25 20:56:51 +0200

    add ch3 test 10

---
+++ LowerToUpper.java
@@ -0,0 +1,10 @@
+public class LowerToUpper {
+    public static void main(String[] args) throws java.io.IOException {
+        char ch;
+        do {
+            ch = (char) System.in.read();
+            ch -= 32;
+            System.out.println(ch);
+        } while (ch != 'Q');
+    }
+}
