bbf548921062282bd9d064b50d781469b4c3d1bd
Simon <trilogicworlds@gmail.com>
2020-06-25 20:48:52 +0200

    add ch3 test 9

---
+++ chapter3/Powers2.java
@@ -0,0 +1,10 @@
+package chapter3;
+
+public class Powers2 {
+    public static void main(String[] args) {
+        
+        for (int i = 1; i < 5; i += i) {
+            System.out.println(i);
+        }
+    }
+}
