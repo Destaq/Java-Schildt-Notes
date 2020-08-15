6cd6d884c7c20d51e5acf70b1f85ab323ed4cc37
Destaq <trilogicworlds@gmail.com>
2020-08-15 15:09:13 +0200

    add static demonstration files

---
+++ chapter6/SDemo3.java
@@ -0,0 +1,25 @@
+class StaticBlock {
+    static double rootof2;
+    static double rootof3;
+
+    // this block is executed when the class is loaded
+    static {
+        System.out.println("Inside static block!");
+        rootof2 = Math.sqrt(2.0);
+        rootof3 = Math.sqrt(3.0);
+    }
+
+    StaticBlock(String msg) {
+        System.out.println(msg);
+    }
+
+}
+
+class SDemo3 {
+    public static void main(String[] args) {
+        StaticBlock ob = new StaticBlock("Inside Constructor!");
+
+        System.out.println("Square root of two is " + StaticBlock.rootof2);
+        System.out.println("Square root of two is " + StaticBlock.rootof3);
+    }
+}
---
+++ chapter6/StaticDemo.java
@@ -0,0 +1,29 @@
+class SDemo {
+    int x; // normal instance variable
+    static int y; // thre is one copy of y, it is global
+
+    int sum() {
+        return x * y;
+    }
+}
+
+public class StaticDemo {
+    public static void main(String[] args) {
+        SDemo ob1 = new SDemo();
+        SDemo ob2 = new SDemo();
+
+        // each object has its own copy of the instance variable
+        ob1.x = 10;
+        ob2.x = 20;
+        System.out.println("Non-static variables are independent!\n" + ob1.x + " " + ob2.x);
+
+        // static variables are shared
+        System.out.println("The static variable is shared!");
+        ob1.y = 92;
+        System.out.println(ob1.sum() + " " + ob2.sum());
+
+        /*
+        Note: you cannot access non-static variables from within a static method.
+         */
+    }
+}
