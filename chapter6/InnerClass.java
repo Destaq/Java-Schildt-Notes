96f30b2b45f88ca2d75718ae2e524d3f1f51bb33
Destaq <trilogicworlds@gmail.com>
2020-08-17 11:02:24 +0200

    add varargs demonstration

---
+++ chapter6/InnerClass.java
@@ -0,0 +1,59 @@
+class Outer {
+    int nums[];
+
+    Outer (int n[]) {
+        nums = n;
+    }
+
+    void analyze() {
+        Inner inOb = new Inner();
+
+        System.out.println("Minimum " + inOb.min());
+        System.out.println("Maximum " + inOb.max());
+        System.out.println("Average " + inOb.avg());
+    }
+
+    // this is an inner class
+    class Inner {
+        int min() {
+            int m = nums[0];
+
+            for (int i = 1; i < nums.length; i++) {
+                if (nums[i] < m) m = nums[i];
+            }
+
+            return m;
+        }
+
+        int max() {
+            int m = nums[0];
+
+            for (int i = 1; i < nums.length; i++) {
+                if (nums[i] > m) m = nums[i];
+            }
+
+            return m;
+        }
+
+        int avg() {
+            int a = 0;
+
+            for (int i = 0; i < nums.length; i++) {
+                a += nums[i];
+            }
+
+            return a/nums.length;
+        }
+
+    }
+
+}
+
+class InnerClass {
+    public static void main(String[] args) {
+        int x[] = { 3, 5, 6, 2, 7, 3, 6, 2, 1, 6 };
+        Outer outOb = new Outer(x);
+
+        outOb.analyze();
+    }
+}
---
+++ chapter6/VarArgsDemo.java
@@ -0,0 +1,22 @@
+public class VarArgsDemo {
+    // supports multiple types of args
+
+    // NOTE: there can only ever be one vararg parameter
+    static void vaTest(String msg, int ... v) {
+        System.out.println(msg);
+        System.out.println("Number of args " + v.length);
+        System.out.println("Contents: ");
+
+        for (int i = 0; i < v.length; i++) {
+            System.out.println(" arg " + i + ": " + v[i]);
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        // var args = variable number of args
+        vaTest("One vararg: ", 1);
+        vaTest("Three varargs: ", 1, 2, 3);
+        vaTest("No varargs.");
+    }
+}
