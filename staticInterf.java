f77a08c71f6b910cd75a307fd8fe990856312aab
Destaq <trilogicworlds@gmail.com>
2020-09-04 19:47:56 +0200

    add static interface

---
+++ staticInterf.java
@@ -0,0 +1,22 @@
+public interface staticInterf {
+    // this is a normal interface method declaration
+    // it does not define a default implementation
+    int getUserId();
+
+    // default method, default implementation, optional
+    default int getAdminID() {
+        return 1;
+    }
+
+    // this is a static interface method
+    static int getUniversalId() {
+        System.out.println(essentiallyNothing());
+        return 0;
+    }
+    // this method can be called directly with `int uID = staticInterf.getUniversalId();`
+
+    private static String essentiallyNothing() {
+        return "Nothing, just a demo";
+    }
+    // this can only be called within the interface as it is private
+}
