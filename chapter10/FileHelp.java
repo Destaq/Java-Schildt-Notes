a293621483538ebca85644908de94b322ddca709
Destaq <trilogicworlds@gmail.com>
2020-11-03 18:45:45 +0100

    add remainder of chapter 10

---
+++ chapter10/FileHelp.java
@@ -0,0 +1,70 @@
+package chapter10;
+
+import java.io.*;
+
+class Help {
+    String helpfile; // name of helpfile
+
+    Help(String fname) {
+        helpfile = fname;
+    }
+
+    boolean helpOn(String what) {
+        int ch;
+        String topic, info;
+        try (BufferedReader helpRdr = new BufferedReader(new FileReader(helpfile))) {
+            do {
+                ch = helpRdr.read();
+
+                if (ch == '#') {
+                    topic = helpRdr.readLine();
+
+                    if (what.compareTo(topic) == 0) {
+                        do {
+                            info = helpRdr.readLine();
+                            if (info != null)
+                                System.out.println(info);
+                        } while ((info != null) && info.compareTo("") != 0);
+                    }
+                }
+            } while (ch != -1);
+        } catch (IOException exc) {
+            System.out.println("Error accessing help file.");
+            return false;
+        }
+        return false; // topic not found
+    }
+
+    String getSelection() {
+        String topic = "";
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.print("Enter topic: ");
+
+        try {
+            topic = br.readLine();
+        } catch (IOException exc) {
+            System.out.println("Error reading console.");
+        }
+
+        return topic;
+    }
+}
+
+public class FileHelp {
+    public static void main(String[] args) {
+        Help hlpobj = new Help("helpfile.txt");
+        String topic;
+
+        System.out.println("Try the help system. " + "Enter 'stop' to end.");
+
+        do {
+            topic = hlpobj.getSelection();
+
+            if (!hlpobj.helpOn(topic)) {
+                System.out.println("Topic not found.\n");
+            }
+        } while (topic.compareTo("stop") != 0);
+    }
+}
